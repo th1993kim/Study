f66580eabf5ff305cf8effa29ad9f130768099ab
th1993kim <th1993kim@gmail.com>
2024-09-18 22:03:18 +0900

    [백준 알고리즘 15894 문제] 완료 (14296KB, 96ms) - 규칙찾기

---
+++ BackjunAlgorithm/src/Y2024M09/P15894.java
@@ -0,0 +1,16 @@
+package Y2024M09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P15894 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long input = Integer.parseInt(br.readLine());
+
+        System.out.println(input * 4);
+    }
+}
