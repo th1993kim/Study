8acbf405abfae3f8d476ca8033c5e7d9b20601af
th1993kim <th1993kim@gmail.com>
2024-09-22 16:46:48 +0900

    [백준 알고리즘 24263 문제] N회 반복 시간복잡도

---
+++ BackjunAlgorithm/src/Y2024M09/P24263.java
@@ -0,0 +1,17 @@
+package Y2024M09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P24263 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int input = Integer.parseInt(br.readLine());
+
+        System.out.println(input);
+        System.out.println(1);
+    }
+}
