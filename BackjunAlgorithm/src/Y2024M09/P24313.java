0adfe97728f296bf87dfcd5dc4336b04a3f57916
th1993kim <th1993kim@gmail.com>
2024-09-24 00:31:38 +0900

    [백준 알고리즘 24313 문제] 그래프로 접근해보기, 기울기로 인한 결과값 나눠보기 (틀림)

---
+++ BackjunAlgorithm/src/Y2024M09/P24313.java
@@ -0,0 +1,29 @@
+package Y2024M09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P24313 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a1 = Integer.parseInt(st.nextToken());
+        int a0 = Integer.parseInt(st.nextToken());
+
+        int c = Integer.parseInt(br.readLine());
+        int n0 = Integer.parseInt(br.readLine());
+        int angle = a1 - c;
+        if (angle > 0) {
+            System.out.println(0);
+        } else {
+            if((a1 * n0 + a0) <= c * n0) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
+}
