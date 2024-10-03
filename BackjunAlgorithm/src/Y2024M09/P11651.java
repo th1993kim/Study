cd8faf43b0ad703f828a5761a78488ef4efa8cb2
th1993kim <th1993kim@gmail.com>
2024-10-03 11:42:36 +0900

    [백준 알고리즘 11651 문제] 11650과 동일한 문제 51336KB, 672ms

---
+++ BackjunAlgorithm/src/Y2024M09/P11651.java
@@ -0,0 +1,39 @@
+package Y2024M09;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P11651 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] twoArr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            twoArr[i][0] = Integer.parseInt(st.nextToken());
+            twoArr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(twoArr, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
+
+            return o1[1] - o2[1];
+        });
+
+        for (int[] oneArr : twoArr) {
+            bw.write(oneArr[0] + " " + oneArr[1] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
