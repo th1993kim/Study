386d955fbc4709744d897adc1598814f976d3f4e
th1993kim <th1993kim@gmail.com>
2024-10-13 23:57:23 +0900

    [백준 알고리즘 1269 문제] 98776KB 832ms 차집합 먼저 구하는 방법도 생각해보기

--- BackjunAlgorithm/src/Y2024M10/P1269.java
+++ BackjunAlgorithm/src/Y2024M10/P1269.java
@@ -37,4 +37,37 @@ public class P1269 {
         bw.close();
         br.close();
     }
+
+    private static void gptSolution(BufferedReader br, BufferedWriter bw) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Set<Integer> aSet = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            aSet.add(Integer.parseInt(st.nextToken()));
+        }
+        Set<Integer> bSet = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            bSet.add(Integer.parseInt(st.nextToken()));
+        }
+
+
+        Set<Integer> aDifferSet = new HashSet<>(aSet);
+        aDifferSet.removeAll(aSet);
+        Set<Integer> bDifferSet = new HashSet<>(bSet);
+        bDifferSet.removeAll(aSet);
+
+        Set<Integer> answerSet = new HashSet<>();
+        answerSet.addAll(aDifferSet);
+        answerSet.addAll(bDifferSet);
+
+        bw.write(String.valueOf(answerSet.size()));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
