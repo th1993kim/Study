c55be0a31dd8ea82eccc21d0c01694656496f2fd
th1993kim <th1993kim@gmail.com>
2024-10-13 23:50:23 +0900

    [백준 알고리즘 1269 문제] 98776KB 832ms

---
+++ BackjunAlgorithm/src/Y2024M10/P1269.java
@@ -0,0 +1,40 @@
+package Y2024M10;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class P1269 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Set<Integer> aSet = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            aSet.add(Integer.parseInt(st.nextToken()));
+        }
+        Set<Integer> bSet = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            bSet.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Set<Integer> answerSet = new HashSet<>();
+        answerSet.addAll(aSet);
+        answerSet.addAll(bSet);
+
+        answerSet.removeIf(number -> aSet.contains(number) && bSet.contains(number));
+
+        bw.write(String.valueOf(answerSet.size()));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
