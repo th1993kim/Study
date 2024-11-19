d392fb28342435816ddff010ba7d4953c66b60da
th1993kim <th1993kim@gmail.com>
2024-11-20 04:31:35 +0900

    [Multi-Module] API 모듈 스프링 설정 추가

--- taehyun-shop-multi-project/module-api/src/main/java/api/ShopSpringBootApplication.java
+++ taehyun-shop-multi-project/module-api/src/main/java/api/ShopSpringBootApplication.java
@@ -2,10 +2,13 @@ package api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@ComponentScan({
+        "api",
+        "core"
+})
 public class ShopSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShopSpringBootApplication.class, args);
