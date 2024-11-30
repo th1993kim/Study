c8b4bd3316745d5f73d1ede3c955450f0d33f763
taehyun <th1993kim@gmail.com>
2024-11-30 22:38:49 +0900

    [백준알고리즘 10845문제] 요세푸스 원형 큐 문제

--- BackjunAlgorithm/src/Y2024M11/P1158.java
+++ BackjunAlgorithm/src/Y2024M11/P1158.java
@@ -37,4 +37,30 @@ public class P1158 {
         bw.close();
         br.close();
     }
+
+    private void otherSolution(BufferedReader br, BufferedWriter bw) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        int count = 1;
+        for (int i = 1; i <= n; i++) {
+            linkedList.add(i);
+        }
+
+        while (!linkedList.isEmpty()) {
+            if (count++ % k == 0) {
+                sb.append(linkedList.remove());
+            } else {
+                linkedList.add(linkedList.remove());
+            }
+        }
+        sb.append(">");
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
