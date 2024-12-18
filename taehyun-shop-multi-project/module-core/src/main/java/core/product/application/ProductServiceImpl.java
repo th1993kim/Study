e7ba0c8183125ff1a8f5b3d6246a3e86f431a0f4
taehyun <th1993kim@gmail.com>
2024-12-18 19:04:45 +0900

    [multi] 테스트 프로파일 수정 및 테스트 코드수정

--- taehyun-shop-multi-project/module-core/src/main/java/core/product/application/ProductServiceImpl.java
+++ taehyun-shop-multi-project/module-core/src/main/java/core/product/application/ProductServiceImpl.java
@@ -3,12 +3,18 @@ package core.product.application;
 import core.product.entity.ProductEntity;
 import core.product.infrastructure.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-@Transactional
+import java.time.LocalDateTime;
+
+@Transactional(isolation = Isolation.REPEATABLE_READ)
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
@@ -22,8 +28,11 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void updateProductPessimisticWriteLock(Long id) {
 
+        log.debug("트랜잭션  활성화 여부: {}", TransactionSynchronizationManager.isActualTransactionActive());
         ProductEntity product = productRepository.findByIdWithPessimisticWriteLock(id);
+        log.debug("조회 종료 :{} 쓰레드 :{}", LocalDateTime.now().getNano(), Thread.currentThread().getName());
         product.decreaseStock();
+        log.debug("수정 종료 :{} 쓰레드 :{}", LocalDateTime.now().getNano(), Thread.currentThread().getName());
     }
 
     @Override
--- taehyun-shop-multi-project/module-core/src/test/java/core/product/application/ProductServiceImplTest.java
+++ taehyun-shop-multi-project/module-core/src/test/java/core/product/application/ProductServiceImplTest.java
@@ -1,11 +1,15 @@
 package core.product.application;
 
 import core.product.entity.ProductEntity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -13,8 +17,11 @@ import java.util.concurrent.Executors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
+@ActiveProfiles("core-test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @Sql(scripts = {"classpath:sql/product.sql"})
+@Slf4j
 class ProductServiceImplTest {
 
     @Autowired
@@ -22,15 +29,16 @@ class ProductServiceImplTest {
 
 
     @Test
-    @Transactional
+    @Rollback
     void updateProductPessimisticReadLock() throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
-        CountDownLatch countDownLatch = new CountDownLatch(2);
-
-        for (int i = 0; i < 2; i++) {
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        CountDownLatch countDownLatch = new CountDownLatch(20);
+        for (int i = 0; i < 20; i++) {
             executorService.execute(() -> {
-                countDownLatch.countDown();
+                log.debug("Update Start CurrentThread = {}", Thread.currentThread());
                 productService.updateProductPessimisticReadLock(1L);
+                log.debug("Update End CurrentThread = {}", Thread.currentThread());
+                countDownLatch.countDown();
             });
         }
 
@@ -38,11 +46,27 @@ class ProductServiceImplTest {
 
         ProductEntity product = productService.findById(1L);
 
-        assertThat(product.getStock()).isEqualTo(18);
+        assertThat(product.getStock()).isEqualTo(0);
     }
 
     @Test
-    void updateProductPessimisticWriteLock() {
+    void updateProductPessimisticWriteLock() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        CountDownLatch countDownLatch = new CountDownLatch(20);
+        for (int i = 0; i < 20; i++) {
+            executorService.execute(() -> {
+                log.debug("Update Start CurrentThread = {}", Thread.currentThread());
+                productService.updateProductPessimisticWriteLock(1L);
+                log.debug("Update End CurrentThread = {}", Thread.currentThread());
+                countDownLatch.countDown();
+            });
+        }
+
+        countDownLatch.await();
+
+        ProductEntity product = productService.findById(1L);
+
+        assertThat(product.getStock()).isEqualTo(0);
     }
 
     @Test
