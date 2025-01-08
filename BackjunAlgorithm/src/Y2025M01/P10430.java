d53c2416d904087fb3154ecabccc3f091572253f
taehyun <th1993kim@gmail.com>
2025-01-08 23:25:06 +0900

    [백준 10430번 문제] 단순 수식 계산

---
+++ BackjunAlgorithm/src/Y2025M01/P10430.java
@@ -0,0 +1,32 @@
+package Y2025M01;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P10430 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String word = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(word);
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append((A + B) % C).append("\n");
+        sb.append(((A % C) + (B % C)) % C).append("\n");
+        sb.append((A * B) % C).append("\n");
+        sb.append(((A % C) * (B % C)) % C).append("\n");
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
