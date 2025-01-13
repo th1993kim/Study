8c53ba540c2a56dbf88c426a450337ccbec53038
taehyun <th1993kim@gmail.com>
2025-01-14 08:29:10 +0900

    [백준 9095, 11727 문제] 동적프로그래밍 문제

---
+++ BackjunAlgorithm/src/Y2025M01/P11727.java
@@ -0,0 +1,30 @@
+package Y2025M01;
+
+import java.io.*;
+
+public class P11727 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        arr[1] = 1;
+        if (n > 1) {
+            arr[2] = 3;
+        }
+
+        for (int i = 3; i <= n; i++) {
+
+            arr[i] = (arr[i-1] + arr[i-2] + arr[i-2]) % 10007;
+
+        }
+
+
+        bw.write(String.valueOf(arr[n]));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
