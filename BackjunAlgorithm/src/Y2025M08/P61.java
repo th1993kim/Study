216bd01d94a4e4710925e37ebe486203790e4827
th1993kim <th1993kim@gmail.com>
2025-08-24 16:11:49 +0900

    [인프런 P61] 피보나치 수열[메모이제이션] 연습

--- BackjunAlgorithm/src/Y2025M08/P61.java
+++ BackjunAlgorithm/src/Y2025M08/P61.java
@@ -3,18 +3,23 @@ package Y2025M08;
 
 public class P61 {
 
+    static int[] fibo;
 
     public int dfs(int i) {
-        if(i==1) return 1;
-        else if (i==2) return 1;
-        else return dfs(i - 1) + dfs(i - 2);
+        if(fibo[i] != 0) return fibo[i];
+        if(i==1) return fibo[i] = 1;
+        else if (i==2) return fibo[i] = 1;
+        else return fibo[i] = dfs(i - 1) + dfs(i - 2);
     }
 
     public static void main(String[] args) {
         P61 main = new P61();
-        int n = 7;
-        for (int i = 1; i <= n; i++) {
-            System.out.print(main.dfs(i) + " ");
+        int n = 45;
+        fibo = new int[n + 1];
+        main.dfs(n);
+
+        for (int i = 1; i <=n; i++) {
+            System.out.println(fibo[i] + " ");
         }
     }
 
