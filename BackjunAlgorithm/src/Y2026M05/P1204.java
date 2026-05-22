9e0cbc94db6fd87ad87133029f5e9cfbc4c1d092
taehyun <th1993kim@gmail.com>
2026-05-23 01:42:30 +0900

    [인프런 1204] 오답 이유 1. point 객체까지 쓸필요 없음 2. clone은 이중배열을 복사하지 않음 3. 단순히 경로를 돌아온다 생각하면 방문 여부만 되돌려주면 됨. 4. 출발점 방문했다는 표기 꼭 하도록 수정

--- BackjunAlgorithm/src/Y2026M05/P1204.java
+++ BackjunAlgorithm/src/Y2026M05/P1204.java
@@ -1,6 +1,5 @@
 package Y2026M05;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class P1204 {
@@ -10,13 +9,6 @@ public class P1204 {
 
     static int[] xMove = {-1, 0, 1, 0};
     static int[] yMove = {0, 1, 0, -1};
-    static class Point {
-        int x, y;
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -29,24 +21,22 @@ public class P1204 {
         }
 
         map[1][1] = 1;
-        solution(new Point(1, 1), map);
+        solution(1, 1);
 
         System.out.println(answer);
     }
 
-    private static void solution(Point point, int[][] beforeMove) {
-        if (point.x == 7 && point.y == 7) {
-            answer ++;
+    private static void solution(int x, int y) {
+        if (x == 7 && y == 7) {
+            answer++;
         } else {
             for (int i = 0; i < 4; i++) {
-                int nx = point.x + xMove[i];
-                int ny = point.y + yMove[i];
-
-                if (nx >= 1 && nx <= 7 && ny >= 1 && ny <= 7 && beforeMove[nx][ny] == 0) {
-                    int[][] newMap = beforeMove.clone();
-                    System.out.println("x : " + point.x + " y : " +  point.y + " nx :" +nx + "ny : " + ny  + " move : ?" + beforeMove[nx][ny]);
-                    newMap[nx][ny] = 1;
-                    solution(new Point(nx, ny), newMap);
+                int nx = x + xMove[i];
+                int ny = y + yMove[i];
+                if (nx >= 1 && nx <= 7 && ny >= 1 && ny <= 7 && map[nx][ny] == 0) {
+                    map[nx][ny] = 1;
+                    solution(nx, ny);
+                    map[nx][ny] = 0;
                 }
             }
         }
