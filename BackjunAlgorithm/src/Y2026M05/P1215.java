22591870c127e8dd8c884df91fd90f7b1482fd34
taehyun <taehyun@bodycodi.com>
2026-05-27 00:36:09 +0900

    [P1215] 씨름 선수 (유사 그리디 기법)

--- BackjunAlgorithm/src/Y2026M05/P1215.java
+++ BackjunAlgorithm/src/Y2026M05/P1215.java
@@ -1,39 +1,46 @@
 package Y2026M05;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class P1215 {
 
+
+    static class Body {
+        int weight, height;
+        Body(int height, int weight) {
+            this.height = height;
+            this.weight = weight;
+        }
+
+        public int compareTo(Body body) {
+            return body.height - this.height;
+        }
+    }
     static int answer = 0;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[][] inputs = new int[n][2];
+        List<Body> bodyList = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            inputs[i][0] = scanner.nextInt();
-            inputs[i][1] = scanner.nextInt();
+            bodyList.add(new Body(scanner.nextInt(), scanner.nextInt()));
         }
 
-        solution(n, inputs);
+        solution(bodyList);
 
         System.out.println(answer);
     }
 
-    private static void solution(int n, int[][] inputs) {
-        for (int i = 0; i < n; i++) {
-            int weight = inputs[i][0];
-            int height = inputs[i][1];
-            boolean isPossible = true;
-            for (int j = 0; j < n; j++) {
-                int compareWeight = inputs[j][0];
-                int compareHeight = inputs[j][1];
-                if (compareWeight > weight && compareHeight > height) {
-                    isPossible = false;
-                    break;
-                }
-            }
-            if (isPossible) {
+    private static void solution(List<Body> bodyList) {
+        bodyList.sort(Body::compareTo);
+
+        int maxWeight = 0;
+        for (Body body : bodyList) {
+            int bodyWeight = body.weight;
+            if (maxWeight < bodyWeight) {
                 answer++;
+                maxWeight = bodyWeight;
             }
         }
     }
