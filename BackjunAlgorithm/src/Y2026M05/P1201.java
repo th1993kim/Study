141c1e52d925234794e3000e4a8d026c40da29a3
taehyun <taehyun@bodycodi.com>
2026-05-30 01:36:01 +0900

    [P1206] 동전 확인 ->  DFS 인프런 강의 보고 시간단축

--- BackjunAlgorithm/src/Y2026M05/P1201.java
+++ BackjunAlgorithm/src/Y2026M05/P1201.java
@@ -1,6 +1,7 @@
 package Y2026M05;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class P1201 {
@@ -10,28 +11,31 @@ public class P1201 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int count = scanner.nextInt();
-        int[] coins = new int[count];
+        Integer[] coins = new Integer[count];
         for (int i = 0; i < count; i++) {
             coins[i] = scanner.nextInt();
         }
         target = scanner.nextInt();
 
-        dfs(0, coins);
+        Arrays.sort(coins, Collections.reverseOrder());
+        dfs(0, 0, coins);
         System.out.println(answer);
     }
 
-    private static void dfs(int level, int[] coins) {
+    private static void dfs(int level, int sum, Integer[] coins) {
 
-        if (target == 0) {
-            answer = Math.min(answer, level);
+        if (sum > target) {
             return;
+        }
+        if (level >= answer) {
+            return;
+        }
+
+        if (sum == target) {
+            answer = level;
         } else {
             for (int i = 0; i < coins.length; i++) {
-                if (target - coins[i] >= 0) {
-                    target -= coins[i];
-                    dfs(level + 1, coins);
-                    target += coins[i];
-                }
+                dfs(level + 1, sum + coins[i], coins);
             }
         }
     }
