022b2f3ed72f1391d7f7bef3abb4302c7bfe48e0
taehyun <taehyun@bodycodi.com>
2026-05-30 09:31:01 +0900

    [P1206] GPT DP 방식의 풀이

--- BackjunAlgorithm/src/Y2026M05/P1201.java
+++ BackjunAlgorithm/src/Y2026M05/P1201.java
@@ -1,12 +1,10 @@
 package Y2026M05;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class P1201 {
 
-    static int answer = Integer.MAX_VALUE;
     static int target;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -17,27 +15,22 @@ public class P1201 {
         }
         target = scanner.nextInt();
 
-        Arrays.sort(coins, Collections.reverseOrder());
-        dfs(0, 0, coins);
-        System.out.println(answer);
+        int[] dp = new int[target + 1];
+
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        Arrays.sort(coins);
+        solution(dp, coins);
+        System.out.println(dp[target]);
     }
 
-    private static void dfs(int level, int sum, Integer[] coins) {
-
-        if (sum > target) {
-            return;
-        }
-        if (level >= answer) {
-            return;
-        }
-
-        if (sum == target) {
-            answer = level;
-        } else {
-            for (int i = 0; i < coins.length; i++) {
-                dfs(level + 1, sum + coins[i], coins);
+    private static void solution(int[] dp, Integer[] coins) {
+        dp[0] = 0;
+        for (int money = 1; money <= target; money++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (money - coins[j] >= 0) {
+                    dp[money] = Math.min(dp[money], dp[money - coins[j]] + 1);
+                }
             }
         }
     }
-
 }
