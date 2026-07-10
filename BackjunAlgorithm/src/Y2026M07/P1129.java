4dfa50d2d770060d3a2e96090bef7a220ab3f65e
taehyun <taehyun@bodycodi.com>
2026-07-10 14:07:20 +0900

    [P1129] 공통원소 구하기 복습 - Two Pointer 유형: HashSet or Two Pointer 핵심: 중복된 인자를 목록에 담아두고 정렬하여 전달 시간복잡도: O(N + M + KlogK) 공간복잡도: O(N + M) 실수: 오름차순 정렬, 투포인터로도 풀수 있다는 것을 인지

--- BackjunAlgorithm/src/Y2026M07/P1129.java
+++ BackjunAlgorithm/src/Y2026M07/P1129.java
@@ -27,20 +27,23 @@ public class P1129 {
     }
 
     private static List<Integer> solution(int n, int[] numbers1, int m, int[] numbers2) {
+
         List<Integer> answer = new ArrayList<>();
-        Set<Integer> numberSet = new HashSet<>();
-
-        for (int i = 0; i < n; i++) {
-            numberSet.add(numbers1[i]);
-        }
-
-        for (int i = 0; i < m; i++) {
-            if (numberSet.contains(numbers2[i])) {
-                answer.add(numbers2[i]);
+        Arrays.sort(numbers1);
+        Arrays.sort(numbers2);
+        int lt = 0, rt = 0;
+        while (lt < n && rt < m) {
+            if (numbers1[lt] == numbers2[rt]) {
+                answer.add(numbers1[lt]);
+                lt++;
+                rt++;
+            } else if (numbers1[lt] < numbers2[rt]){
+                lt++;
+            } else {
+                rt++;
             }
         }
 
-        answer.sort(Comparator.naturalOrder());
         return answer;
     }
 }
