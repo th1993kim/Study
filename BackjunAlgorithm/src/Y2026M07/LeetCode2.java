0b59ea33ba71c409d6afa3f2e196327644f68880
taehyun <taehyun@bodycodi.com>
2026-07-13 22:16:46 +0900

    [리트코드] 두 숫자를 더하세요. - GPT 풀이 자리수를 다음 계산시에 넘겨주는 방법 유형: 재귀, 연결리스트 핵심: 재귀를 통해 노드를 재계산해주는 처리가 필요 시간복잡도: O(N) 공간복잡도: O(N) 실수: 이중포문의 경우 시간초과가 될 수 있기 때문에 Hash를 이용하여 계산해보는것도 효과적일 수 있음

--- BackjunAlgorithm/src/Y2026M07/LeetCode2.java
+++ BackjunAlgorithm/src/Y2026M07/LeetCode2.java
@@ -16,19 +16,26 @@ public class LeetCode2 {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        if (l1 == null) {
-            return l2;
-        } else if (l2 == null) {
-            return l1;
+        addNumbers(l1, l2, 1);
+    }
+
+    private ListNode addNumbers(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null && carry == 0) {
+            return null;
         }
 
-        int sum = l1.val + l2.val;
-        int addVal = sum / 10;
+        int l1Value = l1 == null ? 0 : l1.val;
+        int l2Value = l2 == null ? 0 : l2.val;
 
-        ListNode nextNode = addTwoNumbers(l1.next, l2.next);
-        if (addVal > 0) {
-            nextNode = addTwoNumbers(nextNode, new ListNode(addVal));
-        }
-        return new ListNode(sum % 10, nextNode);
+        int sum = l1Value + l2Value + carry;
+
+        ListNode listNode = new ListNode(sum % 10);
+
+        listNode.next = addNumbers(
+                l1 != null ? l1.next : null,
+                l2 != null ? l2.next : null,
+                sum / 10);
+
+        return listNode;
     }
 }
