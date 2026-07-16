ef088a301e0d638634ae3c7b4b9f55469d869427
taehyun <taehyun@bodycodi.com>
2026-07-16 19:22:36 +0900

    [니트코드] Valid Sudoku 유형: 배열,해시 핵심: 각기 다른 중복 체크 자료구조를 이용하여 행,열,박스에 대한 중복검사를 시행 다른풀이: 배열 SET을 이용한 풀이 시간복잡도: O(N^2) 공간복잡도: O(N^2) 실수: 서브 박스의 키값을 신중히 선택했어야했다. 자칫하면 중복 키 값이 나올 수 있기 때문이다. (*3의 이유는 한쪽의 길이가 최대 2까지만 될 수 있기 때문이다.)

--- BackjunAlgorithm/src/Y2026M07/NeetCode18.java
+++ BackjunAlgorithm/src/Y2026M07/NeetCode18.java
@@ -15,20 +15,20 @@ public class NeetCode18 {
             Set<Character>[] colSetArr = new HashSet[9];
             Set<Character>[] boxSetArr = new HashSet[9];
 
+            for (int i = 0 ; i < 9; i++) {
+                rowSetArr[i] = new HashSet<>();
+                colSetArr[i] = new HashSet<>();
+                boxSetArr[i] = new HashSet<>();
+            }
 
             for (int i = 0; i < 9; i++) {
                 for (int j = 0; j < 9; j++) {
                     char value = board[i][j];
                     if (!Character.isDigit(value)) continue;
-                    if (rowSetArr[i].contains(value)) return false;
-                    rowSetArr[i].add(value);
-
-                    if (colSetArr[j].contains(value)) return false;
-                    colSetArr[j].add(value);
-
+                    if (!rowSetArr[i].add(value)) return false;
+                    if (colSetArr[j].add(value)) return false;
                     int boxKey = (i / 3) * 3 + (j / 3);
-                    if (boxSetArr[boxKey].contains(value)) return false;
-                    boxSetArr[boxKey].add(value);
+                    if (boxSetArr[boxKey].add(value)) return false;
                 }
             }
             return true;
