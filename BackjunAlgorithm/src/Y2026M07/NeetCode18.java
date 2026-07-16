8a40f59c00c92a3a1afc0fec6673469bdb616cfc
taehyun <taehyun@bodycodi.com>
2026-07-16 19:20:51 +0900

    [니트코드] Valid Sudoku 유형: 배열,해시 핵심: 각기 다른 중복 체크 자료구조를 이용하여 행,열,박스에 대한 중복검사를 시행 다른풀이: 배열 SET을 이용한 풀이 시간복잡도: O(N^2) 공간복잡도: O(N^2) 실수: 서브 박스의 키값을 신중히 선택했어야했다. 자칫하면 중복 키 값이 나올 수 있기 때문이다. (*3의 이유는 한쪽의 길이가 최대 2까지만 될 수 있기 때문이다.)

--- BackjunAlgorithm/src/Y2026M07/NeetCode18.java
+++ BackjunAlgorithm/src/Y2026M07/NeetCode18.java
@@ -11,43 +11,24 @@ public class NeetCode18 {
         public boolean isValidSudoku(char[][] board) {
 
 
-            Map<Integer, Set<Integer>> rowMap = new HashMap<>();
-            Map<Integer, Set<Integer>> colMap = new HashMap<>();
-            Map<Integer, Set<Integer>> boxMap = new HashMap<>();
+            Set<Character>[] rowSetArr = new HashSet[9];
+            Set<Character>[] colSetArr = new HashSet[9];
+            Set<Character>[] boxSetArr = new HashSet[9];
 
 
             for (int i = 0; i < 9; i++) {
                 for (int j = 0; j < 9; j++) {
                     char value = board[i][j];
                     if (!Character.isDigit(value)) continue;
-                    int number = Integer.parseInt(String.valueOf(value));
-                    Set<Integer> rowSet = rowMap.get(i);
-                    if (rowSet == null) {
-                        rowSet = new HashSet<>();
-                        rowMap.put(i, rowSet);
-                    }
-                    if (rowSet.contains(number)) return false;
-                    rowSet.add(number);
+                    if (rowSetArr[i].contains(value)) return false;
+                    rowSetArr[i].add(value);
 
-                    Set<Integer> colSet = colMap.get(j);
-                    if (colSet == null) {
-                        colSet = new HashSet<>();
-                        colMap.put(j, colSet);
-                    }
-                    if (colSet.contains(number)) return false;
-                    colSet.add(number);
+                    if (colSetArr[j].contains(value)) return false;
+                    colSetArr[j].add(value);
 
-
-                    int boxKey = (i / 3) * 3 + j / 3;
-                    Set<Integer> boxSet = boxMap.get(boxKey);
-
-                    if (boxSet == null) {
-                        boxSet = new HashSet<>();
-                        boxMap.put(boxKey, boxSet);
-                    }
-
-                    if (boxSet.contains(number)) return false;
-                    boxSet.add(number);
+                    int boxKey = (i / 3) * 3 + (j / 3);
+                    if (boxSetArr[boxKey].contains(value)) return false;
+                    boxSetArr[boxKey].add(value);
                 }
             }
             return true;
