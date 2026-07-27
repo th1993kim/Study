bf670b6ad65105fa6a6f0bf24f328fffe96bd01e
taehyun <taehyun@bodycodi.com>
2026-07-27 19:07:26 +0900

    [니트코드] Implement Trie (Prefix Tree) 유형: 트리 - Trie 핵심: 소문자인 특성을 이용해 서브 노드를 배치하고, null 체크를 통해 서브노드를 무한 생성, 조회, 마지막 node 여부는 상태값으로 관리한다. 시간복잡도: O(N) 공간복잡도: O(M) 실수:

---
+++ BackjunAlgorithm/src/Y2026M07/NeetCode46.java
@@ -0,0 +1,57 @@
+package Y2026M07;
+
+public class NeetCode46 {
+    private static class PrefixTree {
+        private static class TreeNode {
+            private TreeNode[] child = new TreeNode[26];
+            private boolean isEnd;
+        }
+
+        private final TreeNode root;
+
+        public PrefixTree() {
+            root = new TreeNode();
+        }
+
+        public void insert(String word) {
+            TreeNode current = root;
+
+            for (char ch : word.toCharArray()) {
+                int index = ch - 'a';
+
+                if (current.child[index] == null) {
+                    current.child[index] = new TreeNode();
+                }
+
+                current = current.child[index];
+            }
+
+            current.isEnd = true;
+        }
+
+        public boolean search(String word) {
+            TreeNode node = findNode(word);
+
+            return node != null && node.isEnd;
+        }
+
+        public boolean startsWith(String prefix) {
+
+            return findNode(prefix) != null;
+        }
+
+        private TreeNode findNode(String word) {
+            TreeNode current = root;
+
+            for (char ch : word.toCharArray()) {
+                int index = ch - 'a';
+                if (current.child[index] == null) return null;
+
+                current = current.child[index];
+            }
+
+            return current;
+        }
+    }
+
+}
