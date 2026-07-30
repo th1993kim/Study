9fcaf6eec3f778f482b0c0ba09ab14291b1c6611
taehyun <taehyun@bodycodi.com>
2026-07-30 19:21:45 +0900

    [니트코드] Subsets II 유형: DFS, 백트래킹 핵심: 정렬을 통해 DFS시 지금 넣지 않는 요소에 현재 요소와 동일한 값이 들어가는 문제 방지 시간복잡도: O(Catalan(N)) -> 2^(2N)으로 보이지만 가지치기에 의해 상당수가 잘려나간다. 공간복잡도: O(N * Catalan(N)) 실수:

---
+++ BackjunAlgorithm/src/Y2026M07/NeetCode59.java
@@ -0,0 +1,35 @@
+package Y2026M07;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NeetCode59 {
+    static class Solution {
+        List<String> answer;
+        public List<String> generateParenthesis(int n) {
+            answer = new ArrayList<>();
+            dfs(0, 0, new StringBuilder(), n);
+            return answer;
+        }
+
+        private void dfs(int left, int right, StringBuilder sb, int total) {
+            if (right == total) {
+                answer.add(sb.toString());
+                return;
+            }
+
+            if (left < total) {
+                sb.append("(");
+                dfs(left + 1, right, sb, total);
+                sb.deleteCharAt(sb.length() - 1);
+            }
+
+            if (left > right) {
+                sb.append(")");
+                dfs(left, right + 1, sb, total);
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+    }
+
+}
