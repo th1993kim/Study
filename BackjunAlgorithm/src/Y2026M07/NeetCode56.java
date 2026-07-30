b631f413c6eeb44fdef94ebde32a038efe2caf02
taehyun <taehyun@bodycodi.com>
2026-07-30 16:59:28 +0900

    [니트코드] Combination Sum II 유형: DFS, 백트래킹 핵심: 입력값을 정렬, DFS를 통해 탐색하는데, 현재 깊이 이후에서 동일한 값이 나오는 경우 진행하지 않는다. 이유는 현재 값을 선택하나 이후에 선택하나 동일하기 때문이다. 시간복잡도: O(N * 2^N + NlogN) -> 현재조합 복사 * 조합 + 정렬 공간복잡도: O(K + R * K)  -> 스택 깊이 K = T / M + 정답길이 (최대 스택깊이) * 정답 개수 실수:

---
+++ BackjunAlgorithm/src/Y2026M07/NeetCode56.java
@@ -0,0 +1,43 @@
+package Y2026M07;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NeetCode56 {
+    static class Solution {
+
+        private List<List<Integer>> answer;
+        public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+            answer = new ArrayList<>();
+
+            Arrays.sort(candidates);
+            dfs(0, 0, new ArrayList<>(), candidates, target);
+            return answer;
+        }
+
+        private void dfs(int start, int sum, List<Integer> subList, int[] candidates, int target) {
+
+            if (sum == target) {
+                answer.add(new ArrayList<>(subList));
+                return;
+            }
+            if (sum > target) {
+                return;
+            }
+
+            for (int i = start; i < candidates.length; i++) {
+                if (start < i && candidates[i] == candidates[i-1]) {
+                    continue;
+                }
+
+                int number = candidates[i];
+                if (sum + number > target) break;
+                subList.add(number);
+                dfs(i + 1, sum + number, subList, candidates, target);
+                subList.remove(subList.size() - 1);
+            }
+        }
+    }
+
+}
