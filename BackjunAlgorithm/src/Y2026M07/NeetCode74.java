74f6e6ed1f7a2bf24808571125cc7cf6f710edc1
taehyun <taehyun@bodycodi.com>
2026-08-01 22:20:35 +0900

    [니트코드] Number of Islands - DFS로 푸는 방법 유형: 그래프 - DFS 혹은 BFS 핵심: '1' 이면서 방문하지 않은 곳을 체크하면서 이곳과 이어진곳을 BFS 혹은 DFS로 찾아서 방문체크를 해버린다.  다음 DFS ,BFS 발동시 횟수를 증가시키면된다. 시간복잡도: O(N * M) 공간복잡도: O(N * M) 실수:

--- BackjunAlgorithm/src/Y2026M07/NeetCode74.java
+++ BackjunAlgorithm/src/Y2026M07/NeetCode74.java
@@ -6,45 +6,31 @@ import java.util.Queue;
 public class NeetCode74 {
     static class Solution {
         private int[][] dir = new int[][]{{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
-        private boolean[][] visited;
         public int numIslands(char[][] grid) {
 
-            // 그리드배열을 순회하면서 0인것은 지나가고
-            // 1을 만나면 거기서부터 시작하여 이동하면서 1인것들을 방문했다고 색칠해준다.
-            // 이떄 방문한적이 없는 1을 만난 경우 횟수를 1증가시킨다.
-            visited = new boolean[grid.length][grid[0].length];
-            Queue<int[]> queue = new ArrayDeque<>();
             int answer = 0;
             for (int i = 0; i < grid.length; i++) {
                 for (int j = 0; j < grid[0].length; j++) {
-                    if (grid[i][j] == '1' && !visited[i][j]) {
+                    if (grid[i][j] == '1') {
                         answer++;
-                        visited[i][j] = true;
-                        queue.offer(new int[]{i, j});
-
-                        while (!queue.isEmpty()) {
-                            int[] np = queue.poll();
-                            int y = np[0];
-                            int x = np[1];
-
-                            for (int k = 0; k < dir.length; k++) {
-                                int ny = y + dir[k][0];
-                                int nx = x + dir[k][1];
-
-                                if (ny >= 0 && nx >= 0 && ny <= grid.length - 1 && nx <= grid[0].length - 1 && grid[ny][nx] == '1' && !visited[ny][nx]){
-                                    visited[ny][nx] = true;
-                                    queue.offer(new int[]{ny, nx});
-                                }
-
-                            }
-                        }
-
+                        dfs(i, j, grid);
                     }
                 }
             }
 
             return answer;
         }
+        private void dfs(int y, int x, char[][] grid) {
+            grid[y][x] = '0';
+            for (int i = 0; i < dir.length; i++) {
+                int ny = dir[i][0];
+                int nx = dir[i][1];
+                if (nx >= 0 && ny >= 0 && ny < grid.length && nx < grid[0].length && grid[ny][nx] == '1') {
+                    dfs(ny, nx, grid);
+                }
+            }
+        }
+
     }
 
 }
