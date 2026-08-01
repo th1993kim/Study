febdaec07283ed08a8699e678d5f14bf6cbc7afc
taehyun <taehyun@bodycodi.com>
2026-08-01 14:46:22 +0900

    [니트코드] Maximum Subarray  - DP로 풀었을때 유형: 그리디 핵심: 이전까지의 합과 현재 값을 비교해 교환하거나 더해주는 작업을 진행하면된다. 시간복잡도: O(N) 공간복잡도: O(1) 실수:

--- BackjunAlgorithm/src/Y2026M07/NeetCode66.java
+++ BackjunAlgorithm/src/Y2026M07/NeetCode66.java
@@ -5,15 +5,13 @@ public class NeetCode66 {
         public int maxSubArray(int[] nums) {
             // 그냥 막연하게 현재 num까지 더했을때 0 이하면 부분배열을 종료하면 어떨까?
             int answer = Integer.MIN_VALUE;
-            int sum = 0;
-            for (int i = 0; i < nums.length; i++) {
+            int[] dp = new int[nums.length];
+            dp[0] = nums[0];
+            answer = dp[0];
+            for (int i = 1; i < nums.length; i++) {
                 int current = nums[i];
-                if (sum + current < current) {
-                    sum = current;
-                } else {
-                    sum += current;
-                }
-                answer = Math.max(sum, answer);
+                dp[i] = Math.max(current, dp[i-1] + current);
+                answer = Math.max(dp[i], answer);
             }
 
             return answer;
