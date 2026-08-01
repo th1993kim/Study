853f8d71d419cc2928371c29113b8993890df3ac
taehyun <taehyun@bodycodi.com>
2026-08-01 18:55:26 +0900

    [니트코드] Merge Triplets to Form Target 유형: 그리디 핵심: 목표치보다 큰 triplets들은 스킵한다. 이후 작은값들은 max값 비교후 교환한다. 시간복잡도: O(N) 공간복잡도: O(1) 실수:

---
+++ BackjunAlgorithm/src/Y2026M07/NeetCode71.java
@@ -0,0 +1,30 @@
+package Y2026M07;
+
+public class NeetCode71 {
+    static class Solution {
+        public boolean mergeTriplets(int[][] triplets, int[] target) {
+
+            int tx = target[0];
+            int ty = target[1];
+            int tz = target[2];
+
+            int maxX = 0;
+            int maxY = 0;
+            int maxZ = 0;
+            for (int i = 0; i < triplets.length; i++) {
+                int x = triplets[i][0];
+                int y = triplets[i][1];
+                int z = triplets[i][2];
+
+                if (x <= tx && y <= ty && z <= tz) {
+                    maxX = Math.max(maxX, x);
+                    maxY = Math.max(maxY, y);
+                    maxZ = Math.max(maxZ, z);
+                }
+            }
+
+            return tx == maxX && ty == maxY && tz == maxZ;
+        }
+    }
+
+}
