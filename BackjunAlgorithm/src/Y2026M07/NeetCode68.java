f2c5958134a33adbab624239c4cbaedc2ed15eb4
taehyun <taehyun@bodycodi.com>
2026-08-01 16:45:29 +0900

    [니트코드] Jump Game II 유형: 그리디 핵심: 최대 이동거리까지 이동한후 점프 횟수를 늘리고 다시 최대이동거리를 갱신하는 방법, BFS와 흡사하다. 시간복잡도: O(N) 공간복잡도: O(1) 실수:

--- BackjunAlgorithm/src/Y2026M07/NeetCode68.java
+++ BackjunAlgorithm/src/Y2026M07/NeetCode68.java
@@ -3,21 +3,20 @@ package Y2026M07;
 public class NeetCode68 {
     static class Solution {
         public int jump(int[] nums) {
-            int[] dp = new int[nums.length];
-            for (int i = 0; i < dp.length; i++) {
-                dp[i] = Integer.MAX_VALUE;
-            }
-            dp[0] = 0;
+            int jump = 0;
+            int currentEnd = 0;
+            int nextEnd = 0;
+
             for (int i = 0; i < nums.length - 1; i++) {
-                if (dp[i] == Integer.MAX_VALUE) continue; // 현재 위치가 도달하지 못하는 경우 지나쳐야한다.
-                if (nums[i] == 0) continue;
-                int subLength = Math.min(i + nums[i], nums.length - 1);
-                for (int j = i + 1; j <= subLength; j++) {
-                    dp[j] = Math.min(dp[j], dp[i] + 1);
+
+                nextEnd = Math.max(nextEnd, i + nums[i]);
+                if (currentEnd == i) {
+                    jump++;
+                    currentEnd = nextEnd;
                 }
             }
 
-            return dp[nums.length - 1];
+            return jump;
         }
     }
 
