51b5ed0d926a7258110ed899c261acadc2b2fa81
taehyun <taehyun@bodycodi.com>
2026-08-01 18:30:09 +0900

    [니트코드] Hand of Straights 유형: 그리디 핵심: 정렬 후 최소값부터 진행 (작은값부터 시작해야 부분배열을 만들 수 있기 때문이다.) 이후 카드 별로 카운트를 담은 자료구조를 만든 후 카드를 하나씩 꺼내면서 groupSize만큼 카운팅을 하나씩 제거한다. 이때 최초의 카드가 이미 모두 사용된 카드라면 스킵한다. 시간복잡도: O(NlogN) 공간복잡도: O(N) 실수:

---
+++ BackjunAlgorithm/src/Y2026M07/NeetCode70.java
@@ -0,0 +1,36 @@
+package Y2026M07;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NeetCode70 {
+    static class Solution {
+        public boolean isNStraightHand(int[] hand, int groupSize) {
+            Arrays.sort(hand);
+
+            Map<Integer, Integer> countMap = new HashMap<>();
+
+            for (int i = 0; i < hand.length; i++) {
+                countMap.put(hand[i], countMap.getOrDefault(hand[i], 0) + 1);
+            }
+
+            for (int i = 0; i < hand.length; i++) {
+                int firstCard = hand[i];
+                int firstCardCount = countMap.get(firstCard);
+                if (firstCardCount == 0) continue;
+
+                for (int rangeCard = firstCard; rangeCard < firstCard + groupSize; rangeCard++) {
+                    Integer curCardCount = countMap.get(rangeCard);
+
+                    if (curCardCount == null || curCardCount == 0) return false;
+
+                    countMap.put(rangeCard, curCardCount - 1);
+                }
+            }
+
+            return true;
+        }
+    }
+
+}
