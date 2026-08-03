bd8a522c2cbed50029538d0384c66ea8bf3560a7
taehyun <taehyun@bodycodi.com>
2026-08-03 18:54:22 +0900

    [니트코드] Number of Connected Components in an Undirected Graph 유형: Union & Find 핵심: 그룹번호 매칭 후 새로운 그룹번호가 나올때마다 개수체크하여 반환해준다. 시간복잡도: O(V + E) 공간복잡도: O(V) 실수:

---
+++ BackjunAlgorithm/src/Y2026M07/NeetCode83.java
@@ -0,0 +1,46 @@
+package Y2026M07;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NeetCode83 {
+    static class Solution {
+        public int countComponents(int n, int[][] edges) {
+            int[] group = new int[n];
+
+
+
+            for (int i = 0; i < n; i ++) {
+                group[i] = i;
+            }
+
+            for (int i = 0; i < edges.length; i++) {
+                int pre = edges[i][0];
+                int back = edges[i][1];
+                union(pre, back, group);
+            }
+
+            Set<Integer> set = new HashSet<>();
+            for (int i = 0; i < group.length; i++) {
+                set.add(find(i, group));
+            }
+
+            return set.size();
+        }
+
+        private int find(int a, int[] group) {
+            if (group[a] == a) return a;
+
+            return group[a] = find(group[a], group);
+        }
+
+        private void union(int a, int b, int[] group) {
+            int rootA = find(a, group);
+            int rootB = find(b, group);
+            if (rootA == rootB) return;
+
+            group[rootB] = rootA;
+        }
+    }
+
+}
