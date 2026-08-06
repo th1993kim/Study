7f9c7b06b9262e98c698de6feac556e8caf2f487
taehyun <taehyun@bodycodi.com>
2026-08-06 18:50:01 +0900

    [니트코드] Decode Ways 유형: DP 핵심: 단일 숫자를 문자로 바꾸는 경우, 이전숫자와 합쳐 숫자를 만드는경우 두가지 경우의수를 누적시켜 나가면서 합을 구하면 된다. 시간복잡도: O(N) 공간복잡도: O(N) 실수:

---
+++ BackjunAlgorithm/src/Y2026M08/NeetCode87.java
@@ -0,0 +1,31 @@
+package Y2026M08;
+
+public class NeetCode87 {
+
+    static class Solution {
+        public int numDecodings(String s) {
+            int n = s.length();
+            int[] dp = new int[n+1];
+
+            dp[0] = 1;
+            dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+            for (int i = 2; i <= n; i++) {
+                char current = s.charAt(i-1);
+
+                if (current != '0') {
+                    dp[i] += dp[i-1];
+                }
+
+                int twoDigit = Integer.parseInt(s.substring(i-2, i));
+
+                if (twoDigit >= 10 && twoDigit <= 26) {
+                    dp[i] += dp[i-2];
+                }
+            }
+
+            return dp[n];
+        }
+    }
+
+}
