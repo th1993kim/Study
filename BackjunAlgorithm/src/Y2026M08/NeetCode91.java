0756d34af58369fc9a0c0e2a530cfc4428e613a6
taehyun <taehyun@bodycodi.com>
2026-08-07 16:13:56 +0900

    [니트코드] Longest Increasing Subsequence 유형: DP 핵심: DP[i]를 잡을때 전이가 가능한 상태인지 확인해보자. 시간복잡도: O(N * N) 공간복잡도: O(N) 실수: DP를 세울때 다음 계산을 위해 전이가 가능한 상태인지 확인해보자. nums[i] 와 dp[i]를 이용해서 다음것이 계산이 되는지를 확인해보자.

---
+++ BackjunAlgorithm/src/Y2026M08/NeetCode91.java
@@ -0,0 +1,27 @@
+package Y2026M08;
+
+import java.util.Arrays;
+
+public class NeetCode91 {
+    static class Solution {
+        public int lengthOfLIS(int[] nums) {
+
+            int[] dp = new int[nums.length];
+
+            Arrays.fill(dp, 1);
+
+            int max = 0;
+            for (int i = 0; i < dp.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (nums[i] > nums[j]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }
+                max = Math.max(max, dp[i]);
+            }
+
+            return max;
+        }
+    }
+
+}
