db6c38cab8bed0ce7a3be5ae4372b870424fda1b
taehyun <taehyun@bodycodi.com>
2026-08-19 18:51:47 +0900

    [니트코드] Best Time to Buy and Sell Stock with Cooldown 유형: 2차원 DP 핵심: 1. 구매한 주식 보유 OR 주식이 없는 상태에서 새로 구매 2. 구매한 주식 판매 3. 이미 구매한 주식이 없는 상태 혹은 주식을 모두 판매한 상태 를 기준으로 계속 상태를 업데이트 해나아간다면 최종 판매처리한 금액과 주식이 남지 않은 상태에서의 값을 확인해보면 최대 금액을 구할 수 있다. 시간복잡도: O(N) 공간복잡도: O(1) 실수:

---
+++ BackjunAlgorithm/src/Y2026M08/NeetCode105.java
@@ -0,0 +1,24 @@
+package Y2026M08;
+
+public class NeetCode105 {
+    static class Solution {
+        public int maxProfit(int[] prices) {
+            int hold = -prices[0];
+            int sold = Integer.MIN_VALUE;
+            int rest = 0;
+
+            for (int i = 1; i < prices.length; i++) {
+                int prevHold = hold;
+                int prevSold = sold;
+                int prevRest = rest;
+
+                hold = Math.max(prevHold, prevRest -prices[i]);
+                sold = prevHold + prices[i];
+                rest = Math.max(prevRest, prevSold);
+            }
+
+            return Math.max(sold, rest);
+        }
+    }
+
+}
